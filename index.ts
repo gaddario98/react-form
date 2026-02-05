f05d598ce4e3e82bc4debe6b93d6ea4bff997716
gaddario98 <gaddario98@gmail.com>
2026-02-05 16:54:39 +0100

    fix

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-export * from "@gaddario98/react-form";
+export * from "./config";
+export * from "./containers";
+export * from "./FormManager";
+export * from "./FormField";
+export * from "./hooks";
+export * from "./atoms";
+export type * from "./types";
